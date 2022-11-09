5f9dcce3f507c40f2056934a7b557e6f05d73727
Rayhan0Islam0Shagor <raihanislamshagor2@gmail.com>
2022-11-09 15:48:46 +0600

    save button undo, redo added

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -1,30 +1,70 @@
-import '@toast-ui/editor/dist/toastui-editor.css';
+import React, { useEffect, useRef } from "react";
+import { Box, IconButton } from "@mui/material";
+import * as FaIcons from "react-icons/fa";
 
-import { Editor } from '@toast-ui/react-editor';
-import React, { useEffect, useRef, useState } from 'react';
+import { Editor } from "@toast-ui/react-editor";
 
-const TextEditor = () => {
-  const [value, setValue] = useState('');
+const TextEditor = ({ value, setValue }) => {
   const editorRef = useRef(null);
 
-  useEffect(() => {
-    console.log(value);
-  }, [value]);
+  useEffect(() => {}, [value]);
 
   return (
-    <Editor
-      placeholder='Write here...'
-      initialValue={value}
-      previewStyle='vertical'
-      height='600px'
-      initialEditType='wysiwyg'
-      useCommandShortcut={true}
-      ref={editorRef}
-      onChange={() => {
-        setValue(editorRef.current.getInstance().getMarkdown());
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
       }}
-      hideModeSwitch={true}
-    />
+    >
+      <Editor
+        placeholder="Write here..."
+        initialValue={value}
+        previewStyle="vertical"
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        ref={editorRef}
+        onChange={() => {
+          setValue(editorRef.current.getInstance().getMarkdown());
+        }}
+        hideModeSwitch={true}
+      />
+
+      <Box
+        sx={{
+          position: "absolute",
+          top: "5px",
+          right: {
+            xs: "5px",
+            md: "10px",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100px",
+            gap: "10px",
+          }}
+        >
+          <IconButton
+            className="undo-redo-btn"
+            onClick={() => editorRef.current.getInstance().exec("undo")}
+          >
+            <FaIcons.FaUndo />
+          </IconButton>
+
+          <IconButton
+            className="undo-redo-btn"
+            onClick={() => editorRef.current.getInstance().exec("redo")}
+          >
+            <FaIcons.FaRedo />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
